253f6f0b4328c2b9620e7a2782bf357c1b7b89ec
Thbop <75861321+thbop@users.noreply.github.com>
2024-02-29 23:58:53 -0500

    Final commit for day 2

--- Assets/Scripts/Angel/Angel.gd
+++ Assets/Scripts/Angel/Angel.gd
@@ -1,12 +1,10 @@
 extends CharacterBody2D
 @onready var Animator = $Animator
 
-
 const SPEED: int = 100
 
 
 func _physics_process(delta: float) -> void:
-	
 	var toMouse: Vector2 = ( get_global_mouse_position() - position ).normalized()
 	var toMouseRot: float = atan2( toMouse.y, toMouse.x )
 	var toMouseDis: float = position.distance_to( get_global_mouse_position() )
--- Assets/Scripts/Jeff.gd
+++ Assets/Scripts/Jeff.gd
@@ -1,32 +1,35 @@
 extends CharacterBody2D
+
 @onready var Animator = $Animator
+
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
 
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var mousePos: Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-
-	#mousePos = get_viewport().get_mouse_position()
 	
-	#velocity.x = SPEED * sign( mousePos.x - position.x )
-	
-	Animator.flip_h = sign( velocity.x ) != 1
+	var lastCollision: KinematicCollision2D = get_last_slide_collision()
+	if lastCollision:
+		var dirX: int = lastCollision.get_normal().x
+		if dirX:
+			velocity.x = SPEED * dirX
 	
 	if velocity.x != 0:
 		Animator.animation = "walk"
 	else:
 		Animator.animation = "idle"
+	
+	Animator.flip_h = sign( velocity.x ) != 1
 	
 	Animator.play()
 	move_and_slide()
