cdb5267f17ac9a0509e59f7a58431f03acc7b5b3
Thbop <75861321+thbop@users.noreply.github.com>
2024-03-01 20:34:03 -0500

    Misc things + more tiles

--- Assets/Scripts/Angel/Angel.gd
+++ Assets/Scripts/Angel/Angel.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED: int = 100
 const BASESCALE: float = 2.667
 
+var lastPosition: Vector2 = Vector2.ZERO
+var realVelocity: Vector2 = Vector2.ZERO
+
 func _physics_process(_delta: float) -> void:
 	var toMouse: Vector2 = ( get_global_mouse_position() - position ).normalized()
 	var toMouseRot: float = atan2( toMouse.y, toMouse.x )
@@ -11,9 +14,12 @@ func _physics_process(_delta: float) -> void:
 	
 	
 	velocity = toMouse * SPEED * log( toMouseDis )
-	Animator.scale.y = max( BASESCALE * (velocity.length() / 400), BASESCALE )
+	Animator.scale.y = max( BASESCALE * (realVelocity.length() / 8), BASESCALE )
+	Animator.scale.x = -Animator.scale.y + BASESCALE*2
 	
 	rotation = ( toMouseRot + 1.57075 )
 	
 	Animator.play()
 	move_and_slide()
+	realVelocity = position - lastPosition
+	lastPosition = position
