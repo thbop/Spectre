0201a1a10ab0169d86a5aeb4af6d6e7d6faca580
Thbop <75861321+thbop@users.noreply.github.com>
2024-03-02 17:41:39 -0500

    Elevators kind of work

--- Assets/Scripts/Machines/Elevator.gd
+++ Assets/Scripts/Machines/Elevator.gd
@@ -4,13 +4,18 @@ extends Node2D
 @onready var Floor = $Floor
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+const UPSPEED: int = -1400
 
 func _process(delta: float) -> void:
 	#Rope.points[0] = get_global_mouse_position()
 	
 	
-	if not Floor.is_on_floor():
+	if not (Floor.is_on_floor() and Puller.pulling ):
 		Floor.velocity.y += gravity * delta
+	
+	if Puller.pulling:
+		Floor.velocity.y = UPSPEED * delta
+	
 	Floor.move_and_slide()
 	
 	Rope.points[0] = Puller.position
--- Assets/Scripts/Machines/Puller.gd
+++ Assets/Scripts/Machines/Puller.gd
@@ -1,12 +1,21 @@
-extends StaticBody2D
+extends CharacterBody2D
 @onready var W0 = $Wheel0
 @onready var W1 = $Wheel1
 
-var spin: bool = false
-
+var pulling: bool = false
+var cooldown: int = 0
 
 
 func _process(_delta: float) -> void:
-	if spin:
+	var collision: KinematicCollision2D = move_and_collide(Vector2.ZERO, true)
+	if collision:
+		if collision.get_collider().is_in_group("MachineSwitch") and not cooldown:
+			pulling = not pulling
+			cooldown = 30
+	
+	if cooldown:
+		cooldown -= 1
+	
+	if pulling:
 		W0.rotate(0.04)
 		W1.rotate(-0.08)
