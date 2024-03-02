4c1ff064d9935c2b60ed5a2979d7eb08dcfb3540
Thbop <75861321+thbop@users.noreply.github.com>
2024-03-02 16:20:38 -0500

    Walk on elevator

--- Assets/Scripts/Machines/Elevator.gd
+++ Assets/Scripts/Machines/Elevator.gd
@@ -2,8 +2,13 @@ extends Node2D
 @onready var Rope = $Rope
 @onready var Floor = $Floor
 
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _process(delta: float) -> void:
 	#Rope.points[0] = get_global_mouse_position()
+	
+	if not Floor.is_on_floor():
+		Floor.velocity.y += gravity * delta
+	Floor.move_and_slide()
 	
 	Rope.points[-1] = Floor.position
