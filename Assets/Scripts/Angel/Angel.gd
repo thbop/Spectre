16542d8a032ee797101228f47a73b8256492f2cc
Thbop <75861321+thbop@users.noreply.github.com>
2024-03-03 22:55:55 -0500

    huh

--- Assets/Scripts/Angel/Angel.gd
+++ Assets/Scripts/Angel/Angel.gd
@@ -7,17 +7,22 @@ const BASESCALE: float = 2.667
 var lastPosition: Vector2 = Vector2.ZERO
 var realVelocity: Vector2 = Vector2.ZERO
 
+
+
 func _physics_process(_delta: float) -> void:
 	var toMouse: Vector2 = ( get_global_mouse_position() - position ).normalized()
 	var toMouseRot: float = atan2( toMouse.y, toMouse.x )
 	var toMouseDis: float = position.distance_to( get_global_mouse_position() )
-	
+
 	
 	velocity = toMouse * SPEED * log( toMouseDis )
 	Animator.scale.y = max( BASESCALE * (realVelocity.length() / 8), BASESCALE )
 	Animator.scale.x = -Animator.scale.y + BASESCALE*2
 	
 	rotation = ( toMouseRot + 1.57075 )
+	
+
+		
 	
 	Animator.play()
 	move_and_slide()
